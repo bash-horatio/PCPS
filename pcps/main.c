c1a63b7b7dd8a87bf940d53e5763cc7528480e2d
bash-horatio <bash.horatio@gmail.com>
2015-04-20 10:48:52 +0800

    define some macros

---
+++ pcps/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include "pcps.h"
+
+
+PCPS_RET main(int argc, char *argv[])
+{
+
+
+    return PCPS_OK;
+}
---
+++ pcps/pcps.h
@@ -0,0 +1,10 @@
+#ifndef PCPS_H
+#define PCPS_H
+
+
+#define PCPS_RET int
+
+#define PCPS_OK (0)
+#define PCPS_FAIL (-1)
+
+#endif // PCPS_H
